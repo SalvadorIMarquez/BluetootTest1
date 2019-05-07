c16a3db69f45ed9948eb31ab206e49c76a77e6b6
Salvador Marquez <imt.simm@hotmail.com>
2019-05-07 17:29:09 -0500

    initial project

---
+++ BluetootTest1/BluetoothTest1/BluetoothTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothTest1
+//
+//  Created by Salvador Marquez on 5/7/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
